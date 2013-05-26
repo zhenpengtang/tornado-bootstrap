f03c03b8567d7d8c9dcb73023a5f8950de6ba05b
microacup <xiangziyou@hotmail.com>
2013-05-26 17:11:00 +0800

    修改导航条

--- handler/index.py
+++ handler/index.py
@@ -7,5 +7,6 @@ Created on 2013-5-18
 import handler.base
 
 class IndexHandler(handler.base.BaseHandler):
-    def get(self):
-        self.render("index.html")
+     def get(self, template_variables = {}):
+         template_variables["active_page"] = "index"
+         self.render("index.html", **template_variables)
