beaca92a1154ff74988b4922c156038caef41fce
Power <Power@192.168.1.4>
2013-12-31 22:14:05 +0800

    增加输出json的方法

--- handler/base.py
+++ handler/base.py
@@ -31,6 +31,13 @@ class BaseHandler(tornado.web.RequestHandler):
     def render_from_string(self, template_string, **template_vars):
         template = self.jinja2.from_string(template_string)
         self.write(template.render(**template_vars))
-    
+        
+    def render_json(self, data):
+        '''输出json，注意，这里不要使用self.write(dict(data)),不要相信源码里说的，
+                        因为它输出不了汉字，而是输出了编码,所以，这里的参数接受一个dict或者str.
+        '''
+        self.set_header("Content-Type", "application/json; charset=utf-8")
+        self.write(str(data))
+        
     def write_error(self, status_code, **kwargs):
         print('what? error code:' + str(status_code))
